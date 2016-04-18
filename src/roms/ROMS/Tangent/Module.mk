741fbfd64f3be554629f29e2193b3bbc592d68bd
mcflugen <huttone@colorado.edu>
2016-04-18 16:57:30 -0600

    Added ROMS source code.

---
+++ src/roms/ROMS/Tangent/Module.mk
@@ -0,0 +1,15 @@
+# svn $Id: Module.mk 2328 2014-01-23 20:16:18Z arango $
+#::::::::::::::::::::::::::::::::::::::::::::::::::::: Hernan G. Arango :::
+# Copyright (c) 2002-2014 The ROMS/TOMS Group             Kate Hedstrom :::
+#   Licensed under a MIT/X style license                                :::
+#   See License_ROMS.txt                                                :::
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+local_sub  := ROMS/Tangent
+
+local_lib  := libTLM.a
+local_src  := $(wildcard $(local_sub)/*.F)
+
+$(eval $(call make-library,$(local_lib),$(local_src)))
+
+$(eval $(compile-rules))
