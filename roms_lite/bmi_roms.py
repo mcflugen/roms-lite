7e05c0ca902c5b66157ea68497cd64101ba9d4df
mcflugen <huttone@colorado.edu>
2016-04-26 11:25:33 -0600

    Added methods for start, end, and current time.

--- roms_lite/bmi_roms.py
+++ roms_lite/bmi_roms.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+import sys
 import subprocess
 
 
@@ -6,6 +7,9 @@ class RomsLite(object):
     _command = 'mpirun'
     _args = ['-n', '8', 'oceanM', './External/ocean_riverplume2.in']
     _env = None
+
+    def __init__(self):
+        self._time = self.get_start_time()
 
     @property
     def args(self):
@@ -20,6 +24,8 @@ class RomsLite(object):
     def update(self, time):
         subprocess.check_call(self.args, shell=False, env=self._env)
 
+        self._time = self.get_end_time()
+
     def update_until(self, time):
         self.update(time)
 
@@ -31,3 +37,12 @@ class RomsLite(object):
 
     def get_output_var_names(self):
         return ()
+
+    def get_start_time(self):
+        return 0.
+
+    def get_end_time(self):
+        return sys.float_info.max
+
+    def get_current_time(self):
+        return self._time
